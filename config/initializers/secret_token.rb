37324a3e32c79604c869febebd69910107501e51
Lina Qu <lqu@thoughtworks.com>
2012-03-05 15:28:50 -0600

    00 Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CssaLuc::Application.config.secret_token = 'cc5de2fbf49e36e1a3af7fc9fa4d1edef9df1f33e0c8792a625f06db341d4135a161d3e0fef823ee56a3c28473a3224c629ea7ddc0f14ab48d1ab1a15d8bc4fc'
